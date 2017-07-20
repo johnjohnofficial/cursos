1808bcc4a7d2db7fd2d82674b7ed653d4ebb66f9
John Ferreira <jferreira.ba@gmail.com>
2017-07-20 00:14:31 -0300

    ...

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=255)
+
+class Post(models.Model):
+	category = models.ForeignKey(Category)
+	name = models.CharField(max_length=255)
+	content = models.TextField()
+	STATUS_CHOICES = (
+		('Draft', 'Draft'),
+		('Published', 'Published'),
+	)
+	status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Draft')
+	created_at = models.DateTimeField(auto_now_add=True)
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,8 +3,36 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Category, Post
 
 # Create your views here.
 
 def home(request):
-	return HttpResponse("Hello World")
+
+	name = "João"
+	
+	# categories = ['Python', 'PHP', 'Java', 'Ruby']
+	# for category in categories:
+	# 	Category.objects.create(name=category)
+
+	all_categories = Category.objects.all()
+
+	# category_python = Category.objects.get(name='Python')
+
+	# post = Post()
+	# post.name = 'My First very Post'	
+	# post.content = "Content of my first Post"
+	# post.status = "Published"
+	# post.category = category_python
+	# post.save()
+
+	post = Post.objects.get(pk=1)
+
+
+	context = {
+		'name': name,
+		'categories': all_categories,
+		'post':post,
+	}
+
+	return render(request, 'blog/home.html', context)
