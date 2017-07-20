28adebfad3eb95e4ae8cca95853fbb8d61f836da
John Ferreira <jferreira.ba@gmail.com>
2017-07-20 01:33:16 -0300

    ...

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from blog.models import Category, Post
+
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('name', 'status', 'category')
+	search_fields = ['id', 'name', 'content']
+	list_filter = ['status', 'category', 'created_at']
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+	url(r'^$', lambda r: HttpResponseRedirect('blog')),
     url(r'^admin/', admin.site.urls),
     url(r'^blog', include('blog.urls')),
 ]
